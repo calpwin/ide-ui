1bbc81d50327f595a012427392bed03d65440624
irotaev <irotaev@gmaill.com>
2020-01-22 12:34:30 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cide-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'calpvin-ide';
+}
